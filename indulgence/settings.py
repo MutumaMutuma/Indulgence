0719111d15fbe331f62fc41d3fe3463cc4555c14
Yvonne-Ouma <lewismutuma1000@gmail.com>
2019-03-22 15:17:46 -0400

    configured bootstrap

--- indulgence/settings.py
+++ indulgence/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'indulge.apps.IndulgeConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
